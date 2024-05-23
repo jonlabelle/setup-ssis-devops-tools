e5f90dccb756bc03aec3f51cb72e63951be45844
Jon LaBelle <contact@jonlabelle.com>
2024-05-23 16:42:35 -0500

    chore: add commitlint links

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,2 @@
+// commitlint configuration (https://commitlint.js.org/reference/configuration.html#configuration-object-example, https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional)
 module.exports = { extends: ['@commitlint/config-conventional'] };
